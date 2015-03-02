3b944cf878b6dc71224fc8452426c3b9e671854a
Felix Liu <felixliu@berkeley.edu>
2015-03-01 16:11:42 -0800

    successful database test set up

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,7 +1,13 @@
 from django.test import TestCase
 from backend.models import UsersModel
 
-class UsersModelTestCase(self):
+class UsersModelTestCase(TestCase):
 
-	def setUp(self):
-		
+    def setUp(self):
+        UsersModel.objects.create(user="felixliu", first_name="felix", last_name="liu",
+                                  teacher="Mr. Banks", school="Berkeley", period="2",
+                                  email="felixliu@berkeley.edu")
+
+    def test_sanity_test(self):
+    	test_user = UsersModel.objects.get(user="felixliu")
+        self.assertEqual(test_user.first_name, 'felix')
